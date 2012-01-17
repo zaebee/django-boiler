28d607ecfb7c1a2085493da566d4ba9453240f41
zaebee <zaebuntu@gmail.com>
2012-01-18 02:58:04 +0400

    removed unused styles and js

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,2 @@
 $(document).ready(function(){
-$("#form :input[type='text']").tooltip({
-        position: "center right",
-        offset: [-2, 10],
-        effect: "fade",
-        opacity: 0.7
-    });
 });
