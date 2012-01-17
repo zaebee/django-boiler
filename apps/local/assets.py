7f546f839da453ddf8b7f244466a8a428c5221c6
zaebee <zaebuntu@gmail.com>
2012-01-18 02:56:58 +0400

    changed assets

--- apps/local/assets.py
+++ apps/local/assets.py
@@ -3,22 +3,15 @@ from django_assets import Bundle, register
 #Javascript
 register('all_js',
         Bundle('js/main-form.js',
-               'js/script.js',
-               'js/humanmsg.js',),
+               'js/script.js',),
         filter='jsmin',
         output='cache/packed.js')
-
-register('libs_js',
-        Bundle('js/libs/jquery.uniform.min.js',),
-        filter='jsmin',
-        output='cache/packed.libs.js')
 
 #CSS
 register('all_css',
         Bundle('css/base.css',
                'css/form.css',
-               'css/uniform.css',
                'css/style.css',
                'css/humanmsg.css',),
-        filter='yui_css',
+        filter='cssmin',
         output='cache/packed.css')
